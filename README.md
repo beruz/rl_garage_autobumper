1a035e693b86c23e10379a4851dba9c92120dcdb
Berke Eruz <beruz@users.noreply.github.com>
2021-03-03 12:28:46 +0300

    Added first version

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# rl_garage_autobumper
- Rocket League Garage trade bumper using Python and Selenium
+# Rocket League Garage Trade Bumper
+Rocket League Garage trade bumper using Python and Selenium <br>
+I had a lot of trade posts on RL Garage so I wanted to bump my trades automatically instead of clicking bump in each post. This is the first time I am using Selenium.
+For now you have to run the code every 15-20 minutes because I did not add a timer.  
+
+## Usage
+1. Make sure you have Python 3 installed.
+2. Clone repository ```git clone https://github.com/beruz/rl_garage_autobumper``` <br>
+If you want to download Chrome Driver on your own use this link: https://chromedriver.chromium.org/downloads
+4. Using a text editor go to ```data(CHANGE THIS).json``` and change the email and password with your own information.
+5. Rename the file ```data(CHANGE THIS).json``` to ```data.json```
+6. Install dependencies 
+```
+cd to_repository_location
+pip install -r requirements.txt
+```
+or if you are using Anaconda Navigator then from Anaconda Prompt  
+
+```
+cd to_repository_location
+conda install --file requirements.txt 
+```
+## Future Work 
+Add cooldown timer so code can run in the background and bumps the trades every 15 or any desired minute.
+
+## Contribution
+The code can be used for personal usage and comments/feedbacks are welcome!
---
+++ RLG_AutoBumper_Class.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+import json
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+homepage_URL = 'https://rocket-league.com'
+
+def get_credentials():     
+        data = 'data.json'
+        with open(data,'r') as f:
+            user = json.load(f)
+        return user
+
+class Bumper:
+    driver_path = "chromedriver.exe"
+    driver = webdriver.Chrome(executable_path=driver_path)
+    user = {}  
+    
+    def login(self):
+               
+        self.driver.maximize_window()
+        self.driver.get(homepage_URL)
+        self.driver.find_element_by_id('acceptPrivacyPolicy').click()
+        #while(True):
+        user = get_credentials()
+        self.driver.find_element_by_id('header-email').send_keys(user['email'])
+        self.driver.find_element_by_id('header-password').send_keys(user['password'])
+        rememberme = self.driver.find_element_by_id('rememberme')
+        self.driver.execute_script("arguments[0].click();", rememberme)
+        login_btn = self.driver.find_element_by_xpath('.//input[@value="Go"]')
+        login_btn.click()
+          
+    def tradePage(self):
+        notif_btn = self.driver.find_element_by_id('declineNotifications')
+        notif_btn.click()
+        
+        username = self.driver.find_element_by_xpath('/html/body/header/section[1]/div/div[4]/div/a/span')
+        username = username.text.lower()
+        
+        trades_URL = homepage_URL + '/trades/' + username
+        self.driver.get(trades_URL)
+    
+    def bumpTrades(self):
+        trades_column = self.driver.find_element_by_xpath('/html/body/main/div/div/div/div[3]')
+        trades_list = trades_column.find_elements(By.CLASS_NAME,'rlg-trade')
+        
+        for trades in trades_list:
+            bump = trades.find_element_by_xpath('.//button')
+            bump_class = bump.get_attribute("class")
+            if (bump_class == 'rlg-trade__action rlg-trade__bump --bump  is--disabled' ):
+                timetxt = trades.find_element(By.CLASS_NAME, 'rlg-trade__time').text        
+                for word in timetxt.split():
+                    waitTime = 0
+                    if word.isdigit():
+                        ago = int(word)
+                    if word == 'minutes':
+                        waitTime = ago * 60 
+                        break
+                    if word == 'seconds':   
+                        waitTime += ago 
+                        break       
+                waitTime = abs(float(waitTime - 960))        
+                print('wait for: ', waitTime/float(60) , ' minutes')
+                continue
+            else:
+                bump.click()
+                WebDriverWait(self.driver, 5)
+                self.driver.find_element_by_xpath('/html/body/div[2]').click()                   
+                
+def main():
+    bumper = Bumper()
+    bumper.login()
+    bumper.tradePage()
+    bumper.bumpTrades()
+    return 0
+if __name__ == '__main__':
+    main()
+    
